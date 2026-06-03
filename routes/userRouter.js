efb44f92be2c6bf7ab6d4f79d54d09260bf42cab
p902601ij-del <p902601ij@gmail.com>
2026-06-03 17:48:57 -0600

    Creacion de ruta de User

---
+++ routes/userRouter.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../data/models/userModel');
+
+// Crear usuario
+router.post('/', async (req, res) => {
+    try {
+        const newUser = new User(req.body);
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+// Obtener usuarios
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+module.exports = router;
